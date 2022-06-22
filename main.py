e78d3ddc78ce024cd21889b722b01a0b9bb01000
Pawel Daniluk (GDS-ENSP) <pawel.daniluk@bshg.com>
2022-06-22 21:39:33 +0200

    Add call check_data step

--- main.py
+++ main.py
@@ -35,21 +35,30 @@ def go(config: DictConfig):
             },
         )
 
+    preprocessed_artifact_name = "preprocessed_data.csv"
     if "preprocess" in steps_to_execute:
         _ = mlflow.run(
             os.path.join(root_path, "preprocess"),
             "main",
             parameters={
                 "input_artifact": f"{input_artifact_name}:latest",
-                "artifact_name": "preprocessed_data.csv",
+                "artifact_name": preprocessed_artifact_name,
                 "artifact_type": "preprocessed_data",
                 "artifact_description": "Data preprocessed"
             }
         )
 
     if "check_data" in steps_to_execute:
-        ## YOUR CODE HERE: call the check_data step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "check_data"),
+            "main",
+            parameters={
+                "reference_artifact": config["data"]["reference_dataset"],
+                "sample_artifact": f"{preprocessed_artifact_name}:latest",
+                "ks_alpha": config["data"]["ks_alpha"],
+            }
+        )
+
 
     if "segregate" in steps_to_execute:
         ## YOUR CODE HERE: call the segregate step
